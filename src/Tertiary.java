41a05d4a63e12593ff6625864bd26ea63acdbabb
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-09-11 17:39:58 -0600

    Added Tertiary

---
+++ src/Tertiary.java
@@ -0,0 +1,17 @@
+public class Tertiary {
+    public static void main(String[] args) {
+        String thingToYell = "Bees!";
+        yellThing(thingToYell);
+        thingToYell = getAThingToYell("oh boy", " 1", " ahhhhh", 4);
+        yellThing(thingToYell);
+    }
+
+    public static String getAThingToYell(String preface, String epiface, String anotherFace, int aNumber){
+        return preface + " oh nooooo" + epiface + anotherFace + aNumber;
+    }
+
+
+    public static void yellThing(String thing) {
+        System.out.println(thing);
+    }
+}
