ec443f306292ea072716513755bcaf93dd89fa2c
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-11-27 17:39:23 -0700

    Added Error Handling

--- src/Eleven.java
+++ src/Eleven.java
@@ -1,3 +1,9 @@
 public class Eleven {
-    
+    public static void main(String[] args) {
+        String input = "";
+        if (input.isEmpty()) {
+            throw new RuntimeException("Input is empty. Ending early.");
+        }
+        System.out.println(input);
+    }
 }
